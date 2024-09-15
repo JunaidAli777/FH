7bf7214cc043f985037c360a7cd25cfce7e6c879
JunaidAli777 <syedjunaidali.ali@gmail.com>
2024-09-16 00:37:42 +0530

    10th commit

--- src/Index.tsx
+++ src/Index.tsx
@@ -12,8 +12,8 @@ const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<App />}>
             <Route index={true} path='/' element={<Home />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/login' element={<Login />} />
+            <Route path='register' element={<Register />} />
+            <Route path='login' element={<Login />} />
         </Route>
     )
 );
